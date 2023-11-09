7736d2e7468ad577dc321cb88165e943a9dacc58
SehanAF <sehanaf4@gmail.com>
2023-11-09 18:59:06 +0700

    Revisi dengan mencoba menggunakan Hashmap dan TryCatch

--- JavaBasic/TugasArraysSechan/AssigmentBusStation.java
+++ JavaBasic/TugasArraysSechan/AssigmentBusStation.java
@@ -2,51 +2,44 @@ package JavaBasic.TugasArraysSechan;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AssigmentBusStation {
     public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Sukabumi");
+        city.add("Cianjur");
+        city.add("Bandung");
+        city.add("Garut");
+        city.add("Tasik");
+        city.add("Ciamis");
+        city.add("Banjar");
 
-        ArrayList<String> busLine = new ArrayList<>();
-        busLine.add("Sukabumi - Cianjur");
-        busLine.add("Cianjur - Bandung");
-        busLine.add("Bandung - Garut");
-        busLine.add("Garut - Tasik");
-        busLine.add("Tasik - Ciamis");
-        busLine.add("Ciamis - Banjar");
 
-        double[] rates = {10_000, 15_000, 7_500, 5_000, 7_500, 6_000};
+        HashMap<String, Integer> busRates = new HashMap<>();
+        busRates.put("Sukabumi-Cianjur", 10000);
+        busRates.put("Cianjur-Bandung", 15000);
+        busRates.put("Bandung-Garut", 7500);
+        busRates.put("Garut-Tasik", 5000);
+        busRates.put("Tasik-Ciamis", 7500);
+        busRates.put("Ciamis-Banjar", 6000);
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)){
+            System.out.print("Masukkan kota keberangkatan: ");
+            String keberangkatan = input.nextLine();
 
-        System.out.print("Masukan Kota keberangkatan: ");
-        String departureCity = input.nextLine();
+            System.out.print("Masukkan kota tujuan: ");
+            String tujuan = input.nextLine();
 
-        System.out.print("Masukan Kota tujuan: ");
-        String cityDestination = input.nextLine();
-        
-        System.out.print("Apakah penumpang anggota TNI dan POLRI (ya/tidak): ");
-        String whenTNI_Polri = input.nextLine();
+            System.out.print("Apakah penumpang TNI/POLRI? (ya/tidak): ");
+            boolean isTniPolri = input.nextLine().equalsIgnoreCase("ya");
 
-        input.close();
-        
-        // harus belajar oop
-        for (int i = 0; i < busLine.size(); i++) {
-            for (int j = i + 1; j < busLine.size(); j++) {
-                if (busLine.get(i).contains(departureCity) && busLine.get(j).contains(cityDestination)) {
-                    double rate = rates[i] + rates[j];
-                    DecimalFormat decimalFormat = new DecimalFormat("#,###");
-                    if (whenTNI_Polri.equalsIgnoreCase("ya")) {
-                        rate *= 0.9; 
-                    } else if ((busLine.equals("Sukabumi - Banjar") || busLine.equals("Banjar - Sukabumi"))) {
-                        rate *= 0.95; 
-                    }
-                    System.out.println("Harga tiket: " + decimalFormat.format(rate));
-                    break;
-                }
-                
-            }
+
+        } catch (Exception e) {
+            
         }
+        
         
     }
 }
